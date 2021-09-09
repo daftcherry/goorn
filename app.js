130fd942593e4a92b1bbcf05c7482e44b81e2a43
daftcherry <88108801+daftcherry@users.noreply.github.com>
2021-09-10 00:12:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const config = require('./config.js');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || config.serverPort;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
